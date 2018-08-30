1103850f63b23d90228b42efa2dc948cda0e6046
maulitta <maulitta@gmail.com>
2018-08-30 14:55:18 +0300

    Replaced "for(;;)" loop to "for of"

--- checkCanonicalOnPage.js
+++ checkCanonicalOnPage.js
@@ -1,26 +1,23 @@
 javascript: (function () {
-    var currentUrl = document.location;
-    var headLinks = document.getElementsByTagName('link');
-
-    var compareStatusText = {
-        'equal': '\r\nCanonical НЕ совпадает с текущей страницей:\r\n',
-        'notEqual': '\r\nCanonical совпадает с текущей страницей:\r\n'
-    };
-    var message = '';
-
-    for (var i = 0; i < headLinks.length; i++) {
-        if (headLinks[i].getAttribute('rel') == 'canonical') {
-            var resultUrl = headLinks[i].getAttribute('href');
-            var message = '';
-            if (resultUrl != currentUrl) {
-                message += compareStatusText.equal + resultUrl;
-                break;
-            } else {
-                message += compareStatusText.notEqual + resultUrl;
-                break;
-            }
-        }
-    }
-    if (message == '') message = 'Нет каноникала';
-    alert(message);
+	var currentUrl = document.location;
+	var headLinks = document.getElementsByTagName('link');
+	var compareStatusText = {
+		'equal': '\r\nCanonical НЕ совпадает с текущей страницей:\r\n',
+		'notEqual': '\r\nCanonical совпадает с текущей страницей:\r\n' 
+	};
+	var message = '';
+	for (let item of headLinks) {
+		if (item.getAttribute('rel') == 'canonical') {
+			var itemUrl = item.getAttribute('href');
+			if (itemUrl != currentUrl) {
+				message += compareStatusText.equal + itemUrl;
+				break;
+			} else {
+				message += compareStatusText.notEqual + decodeURIComponent(itemUrl);
+				break;
+			}
+		}
+	}
+	if (message == '') message = 'Нет каноникала';
+	alert(message);
 })();
